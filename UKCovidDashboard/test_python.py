9ad581b2c371dbe3337738ea8b0854b7a963a584
Laurence Molloy <laurence.molloy@dtime.ai>
2023-02-18 11:53:20 +0000

    Scheduled Github Action V1.3

--- UKCovidDashboard/test_python.py
+++ UKCovidDashboard/test_python.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+import os
+
+script_path = os.path.realpath(os.path.dirname(__file__))
+parent_path = os.path.normpath(os.path.join(script_path, os.pardir))
+file_path = os.path.join(parent_path, 'docs', 'images', 'out.txt')
 time = datetime.utcnow().strftime("%H:%M:%S")
-with open('out.txt', 'w') as file:
+with open(file_path, 'w') as file:
     file.write(time)
