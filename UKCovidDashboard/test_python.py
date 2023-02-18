0da7a6c27ffea444956adf75bcbd57e951ec7eee
Laurence Molloy <laurence.molloy@dtime.ai>
2023-02-18 21:42:21 +0000

    Scheduled Github Action V1.28: deploy to main

--- UKCovidDashboard/test_python.py
+++ UKCovidDashboard/test_python.py
@@ -3,8 +3,8 @@ import os
 
 script_path = os.path.realpath(os.path.dirname(__file__))
 parent_path = os.path.normpath(os.path.join(script_path, os.pardir))
-file_path = os.path.join('..','docs', 'images', 'out.txt')
+file_path = os.path.join(parent_path,'docs', 'images', 'out.txt')
 print(file_path)
 time = datetime.utcnow().strftime("%H:%M:%S")
-with open('docs/images/out.txt', 'w') as file:
+with open(file_path, 'w') as file:
     file.write(time)
