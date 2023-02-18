a7bbe136819bf39eac402ce9fd5bc8cc73f4bf95
Laurence Molloy <laurence.molloy@dtime.ai>
2023-02-18 15:42:08 +0000

    Scheduled Github Action V1.12: path adjustment

--- UKCovidDashboard/test_python.py
+++ UKCovidDashboard/test_python.py
@@ -5,5 +5,5 @@ script_path = os.path.realpath(os.path.dirname(__file__))
 parent_path = os.path.normpath(os.path.join(script_path, os.pardir))
 #file_path = os.path.join('..','docs', 'images', 'out.txt')
 time = datetime.utcnow().strftime("%H:%M:%S")
-with open('out.txt', 'w') as file:
+with open('../docs/images/out.txt', 'w') as file:
     file.write(time)
