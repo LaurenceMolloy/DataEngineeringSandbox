215b03fa11c68b2049048f2b5c68632631808831
Laurence Molloy <laurence.molloy@dtime.ai>
2023-02-18 22:06:13 +0000

    Scheduled Github Action V1.28: file location

--- UKCovidDashboard/test_python.py
+++ UKCovidDashboard/test_python.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import os
 
-script_path = os.path.realpath(os.path.dirname(__file__))
-parent_path = os.path.normpath(os.path.join(script_path, os.pardir))
+#script_path = os.path.realpath(os.path.dirname(__file__))
+parent_path = os.path.normpath(os.path.join('.', os.pardir))
 file_path = os.path.join(parent_path,'docs', 'images', 'out.txt')
 print(file_path)
 time = datetime.utcnow().strftime("%H:%M:%S")
